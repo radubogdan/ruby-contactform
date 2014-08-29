07face4458deb9914be77ccb57e7893fbbe3798d
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-08-29 23:59:16 +0300

    add mandrill, finish first working version

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'json'
 require 'redis'
 require 'uuid'
+require 'mandrill'
 
 r = Redis.new
 r.select 1
@@ -17,11 +18,36 @@ post '/user/register' do
   email = params[:email]
   not_exist = r.sadd "emailset", email
 
-  if email && valid_email?(email) && not_exist
+  if !not_exist
+    "Email already registered, 403"
+  elsif email && valid_email?(email)
     uuid = SecureRandom.uuid
     r.hset "emails", uuid, email
     "Your token for #{email} is #{uuid}"
   else
-    "Email already exist or invalid email was given"
+    "Invalid Email address"
+  end
+end
+
+post '/user/:token' do
+  # --data is email, name, message
+  user_email = (r.hmget "emails", params[:token]).join
+
+  if !user_email
+    "User not found: 406"
+  else
+    mandrill_api_key = ENV["MANDRILL_APIKEY"]
+    m = Mandrill::API.new(mandrill_api_key)
+
+    message = {
+      :to => [{ :email => user_email }],
+      :from_email => params[:email],
+      :subject => params[:subject],
+      :from_name => "Message from #{params[:name]}",
+      :text => params[:message]
+    }
+
+    sending = m.messages.send message
+    sending[0]["status"] == 'sent' ? "Message sent" : "Reject reason: #{sending[0]["reject_reason"]}"
   end
 end
